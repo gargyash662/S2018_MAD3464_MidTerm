d8d376de87525aa98372f30b134cedd355b270ca
yash <gargyash662@gmail.com>
2018-06-07 20:00:18 -0400

    reverse final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,11 @@
 public class Main{
-    
+
+        public static void main (String[] args)
+        {
+                LambtonStringTools r1 = new LambtonStringTools();
+               r1.reverse("Lambton");
+
+        }
+
         }
 
