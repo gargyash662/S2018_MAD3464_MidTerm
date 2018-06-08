3aaa210b2247a94eae7489f9a31179bc2dffa33d
yash <gargyash662@gmail.com>
2018-06-07 20:57:50 -0400

    final commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,15 @@ public class Main{
 
         public static void main (String[] args)
         {
+
                 LambtonStringTools r1 = new LambtonStringTools();
                r1.reverse("Lambton");
 
+             /*  LambtonStringTools r2 = new LambtonStringTools();
+                r2.decimal(""); */
+
+                LambtonStringTools r3 =new LambtonStringTools();
+                r3.convert("hello");
         }
 
         }
--- src/main/java/ReverseString.java
+++
@@ -1,4 +0,0 @@
-public class ReverseString
-{
-    //this is main class
-}
