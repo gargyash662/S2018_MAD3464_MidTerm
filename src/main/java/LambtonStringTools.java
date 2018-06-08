59e0f741ab6280e8f7240a4983fabc44b6172ce5
yash <gargyash662@gmail.com>
2018-06-07 20:18:04 -0400

    decimaltoint

--- src/main/java/LambtonStringTools.java
+++ src/main/java/LambtonStringTools.java
@@ -18,7 +18,26 @@ public class LambtonStringTools {
     {
 
 
-        
+        {
+            int count;
+            String s = "lamton college";
+            String[] split = s.split(" ");
+            for(String str: split)
+            {
+                char[] ch = str.toCharArray();   // convert string to char array
+                count = 0;                       // reset count for every new word/string
+                for(char c: ch)                  // iterate over all the characters
+                {
+                    if(Character.isLetter(c))    // Returns true if the character is a Letter
+                    {
+                        count++;                 // increase the count to represent no. of letters
+                    }
+                }
+                System.out.print(count + " ");
+                System.out.print(ch + " ");// print the no.of characters that are letters in a word/string.
+            }
+        }
+
         return null;
     }
 }
